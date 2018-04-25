b1faecac0f6d25afcc80e04524f6545bb83ffe98
huong123456789 <37567795+huong123456789@users.noreply.github.com>
2018-04-26 03:15:29 +0700

    Add files via upload

---
+++ Dataprovider.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    public class Dataprovider
+    {
+        private static Dataprovider instance;
+        public static Dataprovider Instance
+        {
+            get { if (instance == null)instance = new Dataprovider(); return Dataprovider.instance;}
+            private set { Dataprovider.instance = value; }
+        }
+        private Dataprovider(){}
+        private static string connectionstr = @"Data Source=DESKTOP-0E9SPH3\SQLEXPRESS;Initial Catalog=thu_chi;Integrated Security=True";
+        public DataTable ExecuteQuery1(string query, object[] parameter)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionstr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listPara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listPara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(connectionstr))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+        public int ExecutenonQuery(string query, object[] parameter = null)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionstr))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listpara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listpara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+                data = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return data;
+        }
+        public object ExecuteScalar(string query, object[] parameter = null)
+        {
+            object data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionstr))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+                if (parameter != null)
+                {
+                    string[] listpara = query.Split(' ');
+                    int i = 0;
+                    foreach (string item in listpara)
+                    {
+                        if (item.Contains('@'))
+                        {
+                            command.Parameters.AddWithValue(item, parameter[i]);
+                            i++;
+                        }
+                    }
+
+                }
+                data = command.ExecuteScalar();
+                connection.Close();
+            }
+            return data;
+        }
+       
+       
+    }
+}
